e2494929a314dc6da3b7d4be5646d3bcc203cb35
Andrea Veneziano <andrea.veneziano@epfl.ch>
2020-10-07 17:21:22 +0200

    Try to show map

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,6 +1,34 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
+import React, { useEffect } from 'react';
 
 export default function MapPage(props) {
-  return (<Typography >Map</Typography>);
+  //   useEffect(() => {
+  //     const script = document.createElement('script');
+
+  //     // script.src = 'showMap.js';
+  //     script.async = true;
+  //     script.innerHTML = `
+  //     Mapwize.apiKey('1f04d780dc30b774c0c10f53e3c7d4ea');
+
+  // Mapwize.map({
+  //   container: 'mapwize',
+  // })
+  //   .then((mapInstance) => {
+  //     console.log('Maps is now ready to be used');
+  //   })
+  //   .catch((err) => {
+  //     // Something bad happened during Mapwize loading
+  //     console.error(err);
+  //   });
+  // `;
+
+  //     document.body.appendChild(script);
+
+  //     return () => {
+  //       document.body.removeChild(script);
+  //     };
+  //   }, []);
+
+  return <div id="mapwize">
+    
+  </div>;
 }
