390133146537dd96887eef506d39648b8a1e9592
Andrea Veneziano <andrea.veneziano@epfl.ch>
2020-10-10 21:58:10 +0200

    Add api functions for getting items

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Typography } from '@material-ui/core';
+import { useQuery } from 'react-query';
+
+import { getItems, getItem } from '../api/items';
 
 export default function HomePage() {
- 
-  return (<Typography >Home</Typography>);
+  const { data: items } = useQuery('items', getItems);
+
+  const { data: item, error, isError } = useQuery(['item', 2], getItem);
+
+  if (isError) {
+    console.log(error.message);
+  }
+
+  console.log(items);
+
+  console.log(item);
+
+  return <Typography>Home</Typography>;
 }
