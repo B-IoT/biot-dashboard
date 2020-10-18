d02273cb5bdd86a00614749fabe91713bfc9e89e
Andrea Veneziano <andrea.veneziano@epfl.ch>
2020-10-18 12:28:26 +0200

    Add charts

--- src/pages/MaintenancePage.js
+++ src/pages/MaintenancePage.js
@@ -5,7 +5,20 @@ import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import SearchBar from 'material-ui-search-bar';
 
-import { PieChart, Pie, Tooltip, Cell, BarChart } from 'recharts';
+import {
+  PieChart,
+  Pie,
+  Tooltip,
+  Cell,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  LineChart,
+  Line,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
 
 import ItemsCard from '../components/ItemsCard';
 
@@ -30,22 +43,10 @@ export default function MaintenancePage() {
 
   const [query, setQuery] = useState('');
 
-  const items1 = [
-    { type: 'Weelchair' },
-    { type: 'Oxygen' },
-    { type: 'Oxygen' },
-    { type: 'Oxygen' },
-    { type: 'Oxygen' },
-    { type: 'Oxygen' },
-    { type: 'Oxygen' },
-    { type: 'Oxygen' },
-    { type: 'Oxygen' },
-    { type: 'Oxygen' },
-  ];
-
-  const items2 = [
+  const items = [
     {
       type: 'X',
+      service: 'Bloc 1',
       id: 1,
       battery: 50,
       status: 'Available',
@@ -55,11 +56,23 @@ export default function MaintenancePage() {
     {
       type: 'Y',
       id: 2,
+      service: 'Bloc 2',
       battery: 30,
       status: 'Available',
       latitude: 6.6,
       longitude: 46.4,
     },
+  ];
+
+  const services = [
+    { name: 'Bloc 1', available: 75, needMaintenance: 20, unavailable: 5 },
+    { name: 'Bloc 2', available: 75, needMaintenance: 15, unavailable: 10 },
+  ];
+
+  const dataForTimeSeries = [
+    { nbItemsDamaged: 30, nbItemsRepaired: 20, date: '18/10' },
+    { nbItemsDamaged: 20, nbItemsRepaired: 40, date: '20/10' },
+    { nbItemsDamaged: 5, nbItemsRepaired: 55, date: '24/10' },
   ];
 
   const typeSummaries = [
@@ -103,6 +116,7 @@ export default function MaintenancePage() {
         direction="row"
         justify="space-around"
         alignItems="center"
+        style={{ marginBottom: 32 }}
       >
         <PieChart width={350} height={350}>
           <Pie data={typeSummaries} dataKey="count" nameKey="type" label>
@@ -112,9 +126,49 @@ export default function MaintenancePage() {
           </Pie>
           <Tooltip />
         </PieChart>
-        <ItemsCard title="Maintenance du matériel" items={items1} />
-        <ItemsCard title="Matériel endommagé" items={items2} />
+        <ItemsCard title="Maintenance du matériel" items={items} />
+        <BarChart width={350} height={350} data={services} layout="vertical">
+          <XAxis type="number" hide={true} domain={[0, 100]} />
+          <YAxis type="category" dataKey="name" />
+          <Tooltip />
+          <Bar
+            dataKey="available"
+            fill={theme.items.available}
+            layout="vertical"
+            label
+          />
+          <Bar
+            dataKey="unavailable"
+            fill={theme.items.unavailable}
+            layout="vertical"
+            label
+          />
+          <Bar
+            dataKey="needMaintenance"
+            fill={theme.palette.secondary.main}
+            layout="vertical"
+            label
+          />
+        </BarChart>
       </Grid>
+      <ResponsiveContainer width="100%" height={350}>
+        <LineChart data={dataForTimeSeries} margin={{left: 0, right: 20}}>
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="nbItemsDamaged"
+            stroke={theme.items.unavailable}
+          />
+          <Line
+            type="monotone"
+            dataKey="nbItemsRepaired"
+            stroke={theme.items.available}
+          />
+        </LineChart>
+      </ResponsiveContainer>
     </Grid>
   );
 }
