00e75587cfe99996529472e0375d40a5661636f3
LudoHoff <ludo.hoffstetter@epfl.ch>
2021-02-19 13:42:13 +0100

    Change background color

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ItemButton from '../../components/ItemButton/ItemButton';
 import SearchBar from '../../components/SearchBar/SearchBar';
+import ItemButton from '../../components/ItemButton/ItemButton';
+import PlaceholderButton from '../../components/ItemButton/PlaceholderButton';
 
 import ecgIcon from '../../img/ecgIcon.svg';
 import bedIcon from '../../img/bedIcon.svg';
@@ -9,7 +10,6 @@ import oxygenIcon from '../../img/oxygenIcon.svg';
 import logo from '../../img/logo.png';
 
 import './SearchPage.css';
-import PlaceholderButton from '../../components/ItemButton/PlaceholderButton';
 
 function SearchPage(props) {
   const {
@@ -22,52 +22,47 @@ function SearchPage(props) {
   } = props;
 
   return (
-    <container-center-horizontal>
-      <div className="search-page screen ">
-        <h1 className="search-title axiforma-bold-blue-70px">
-          {' '}
-          {searchTitle}{' '}
-        </h1>
-        <SearchBar searchText={searchText} />
-        <div className="result-grid">
-          <div className="auto-flex">
-            <Link to="/tracking" style={{ textDecoration: 'none' }}>
-              <ItemButton text={ecgText} icon={ecgIcon} />
-            </Link>
-            <PlaceholderButton text={placeHolder} />
-            <PlaceholderButton text={placeHolder} />
-            <PlaceholderButton text={placeHolder} />
-            <PlaceholderButton text={placeHolder} />
-          </div>
-          <div className="auto-flex">
-            <Link to="/tracking" style={{ textDecoration: 'none' }}>
-              <ItemButton text={bedText} icon={bedIcon} />
-            </Link>
-            <PlaceholderButton text={placeHolder} />
-            <PlaceholderButton text={placeHolder} />
-            <PlaceholderButton text={placeHolder} />
-            <PlaceholderButton text={placeHolder} />
-          </div>
-          <div className="auto-flex">
-            <Link to="/tracking" style={{ textDecoration: 'none' }}>
-              <ItemButton text={oxygenText} icon={oxygenIcon} />
-            </Link>
-            <PlaceholderButton text={placeHolder} />
-            <PlaceholderButton text={placeHolder} />
-            <PlaceholderButton text={placeHolder} />
-            <PlaceholderButton text={placeHolder} />
-          </div>
-          <div className="auto-flex">
-            <PlaceholderButton text={placeHolder} />
-            <PlaceholderButton text={placeHolder} />
-            <PlaceholderButton text={placeHolder} />
-            <PlaceholderButton text={placeHolder} />
-            <PlaceholderButton text={placeHolder} />
-          </div>
-          <img className="logo" src={logo} alt="BIoT logo" />
+    <div className="search-page screen ">
+      <h1 className="search-title axiforma-bold-blue-70px">{searchTitle}</h1>
+      <SearchBar searchText={searchText} />
+      <div className="result-grid">
+        <div className="auto-flex">
+          <Link to="/tracking" style={{ textDecoration: 'none' }}>
+            <ItemButton text={ecgText} icon={ecgIcon} />
+          </Link>
+          <PlaceholderButton text={placeHolder} />
+          <PlaceholderButton text={placeHolder} />
+          <PlaceholderButton text={placeHolder} />
+          <PlaceholderButton text={placeHolder} />
         </div>
+        <div className="auto-flex">
+          <Link to="/tracking" style={{ textDecoration: 'none' }}>
+            <ItemButton text={bedText} icon={bedIcon} />
+          </Link>
+          <PlaceholderButton text={placeHolder} />
+          <PlaceholderButton text={placeHolder} />
+          <PlaceholderButton text={placeHolder} />
+          <PlaceholderButton text={placeHolder} />
+        </div>
+        <div className="auto-flex">
+          <Link to="/tracking" style={{ textDecoration: 'none' }}>
+            <ItemButton text={oxygenText} icon={oxygenIcon} />
+          </Link>
+          <PlaceholderButton text={placeHolder} />
+          <PlaceholderButton text={placeHolder} />
+          <PlaceholderButton text={placeHolder} />
+          <PlaceholderButton text={placeHolder} />
+        </div>
+        <div className="auto-flex">
+          <PlaceholderButton text={placeHolder} />
+          <PlaceholderButton text={placeHolder} />
+          <PlaceholderButton text={placeHolder} />
+          <PlaceholderButton text={placeHolder} />
+          <PlaceholderButton text={placeHolder} />
+        </div>
+        <img className="logo" src={logo} alt="BIoT logo" />
       </div>
-    </container-center-horizontal>
+    </div>
   );
 }
 
