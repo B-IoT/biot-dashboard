b6665e0849bb604f5e2b40e04dee4832e3787942
LudoHoff <ludo.hoffstetter@epfl.ch>
2021-03-11 16:57:37 +0100

    Sanitize text input of the floor box

--- src/components/RoundInput/RoundInput.tsx
+++ src/components/RoundInput/RoundInput.tsx
@@ -12,6 +12,17 @@ export default function RoundInput(props: {
     setLocalInput('' + props.input);
   }, [props.input]);
 
+  const parseFloor = () => {
+    // Remove every character except digits
+    const digits = localInput.replace(/\D/g, '');
+    if (digits.length > 0) {
+      props.setInput(parseInt(digits, 10));
+      setLocalInput(digits);
+    } else {
+      setLocalInput('' + props.input);
+    }
+  };
+
   return (
     <div className="round-input">
       <img className="round-input-img" src={roundInput} alt="Round input" />
@@ -24,9 +35,7 @@ export default function RoundInput(props: {
           className="round-input-text axiforma-book-normal-blue-30px"
           value={localInput}
           onChange={(e) => setLocalInput(e.target.value)}
-          onKeyPress={(e) =>
-            e.key === 'Enter' ? props.setInput(parseInt(localInput, 10)) : null
-          }
+          onKeyPress={(e) => (e.key === 'Enter' ? parseFloor() : null)}
         />
       </OutsideAlerter>
     </div>
