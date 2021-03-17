819c3ab8a64976d539fc509bb21a81f383abbd7a
LudoHoff <ludo.hoffstetter@epfl.ch>
2021-03-17 13:33:56 +0100

    Refactor UserMarker

--- src/components/ItemMap/ItemMap.tsx
+++ src/components/ItemMap/ItemMap.tsx
@@ -87,25 +87,6 @@ function ItemMap(props: { itemName: string }) {
   //   setViewport(newViewport);
   // }
 
-  useEffect(() => {
-    if (navigator.geolocation) {
-      setInterval(
-        () =>
-          navigator.geolocation.getCurrentPosition(
-            function (position) {
-              setUserLon(position.coords.longitude);
-              setUserLat(position.coords.latitude);
-              setUserFetched(true);
-            },
-            () => null,
-            //(e) => console.log('Geolocation failed ' + e.code + ' ' + e.message),
-            { enableHighAccuracy: false, timeout: 2000, maximumAge: 2000 }
-          ),
-        2000
-      );
-    }
-  }, []);
-
   const { data } = useQuery('items', getItems, { refetchInterval: 1000 });
   useEffect(() => {
     if (data !== undefined) {
@@ -183,10 +164,6 @@ function ItemMap(props: { itemName: string }) {
               'mapbox://styles/ludohoffstetter/cklfuba923yaa17miwvtmd26g'
             }
           >
-            {markers}
-            {userFetched && (
-              <UserMarker longitude={userLon} latitude={userLat} />
-            )}
             <Source
               id="map-source"
               type="image"
@@ -204,6 +181,14 @@ function ItemMap(props: { itemName: string }) {
               type="raster"
               paint={{ 'raster-opacity': 0.5 }}
             />
+            {markers}
+            <UserMarker
+              userLon={userLon}
+              userLat={userLat}
+              setUserLon={setUserLon}
+              setUserLat={setUserLat}
+              setUserFetched={setUserFetched}
+            />
           </ReactMapGl>
           <div className="mask-edges clear" />
           <div className="blurred-edges clear" />
@@ -218,11 +203,13 @@ function ItemMap(props: { itemName: string }) {
       </div>
 
       <div className="map-control-right">
-        <RoundButton
-          iconPath={'navbarIcons/center.svg'}
-          onClickHandler={centerHandler}
-        />
-        <div className="separator" />
+        {userFetched && (
+          <RoundButton
+            iconPath={'navbarIcons/center.svg'}
+            onClickHandler={centerHandler}
+          />
+        )}
+        {userFetched && <div className="separator" />}
         <RoundButton
           iconPath={'navbarIcons/zoom.svg'}
           onClickHandler={zoomHandler}
